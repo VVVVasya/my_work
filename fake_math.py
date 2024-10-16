584dd9313b3d71d52fb99eab3909524b0487a1b1
VVVVasya <byjor44@gmail.com>
2024-10-16 22:56:40 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,12 @@
+# Задача "А как делить?" к ( module_4_1)
+# В fake_math создайте функцию divide, которая
+# принимает два параметра first и second.
+# Функция должна возвращать результат деления first
+# на second, но когда в second записан 0 - возвращать строку 'Ошибка'.
+
+def divide(first, second):
+    if second == 0:
+        result = 'Ошибка'
+    else:
+        result = first / second
+    return result
---
+++ true_math.py
@@ -0,0 +1,15 @@
+# Задача "А как делить?" к (module_4_1)
+import math
+# В true_math создайте функцию divide,
+# которая принимает два параметра first и second.
+# Функция должна возвращать результат деления first
+# на second, но когда в second записан 0 - возвращать бесконечность.
+# Бесконечность можно импортировать из встроенной
+# библиотеки math (from math import inf)
+from math import inf
+def divide(first, second):
+    if second == 0:
+        result = inf
+    else:
+        result = first / second
+    return result
